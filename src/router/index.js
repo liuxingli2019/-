904d0a95e2f6b8ae7b57f7fc20dedcc6ae7c751f
liu xing li <326667693@qq.com>
2020-06-05 18:42:07 +0800

    首页开始

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,44 @@
 import Vue from 'vue'
-import Router from 'vue-router'
+import VueRouter from 'vue-router'
+
+Vue.use(VueRouter)
 
 
-Vue.use(Router)
+const Home = () => import('../views/home/Home')
+const Category = () => import('../views/category/Category')
+const Cart = () => import('../views/cart/Cart')
+const Profile = () => import('../views/profile/Profile')
 
-export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
-  ]
+
+
+const routes = [
+  {
+  path: '/',
+  redirect: Home
+  
+  },
+  {
+    path: '/home',
+    component: Home
+  },
+  {
+    path: '/cart',
+    component: Cart
+  },
+  {
+    path: '/category',
+    component: Category
+  },
+  {
+    path: '/profile',
+    component: Profile
+  }
+]
+
+const router = new VueRouter({
+  routes,
+  mode: 'history'
 })
+
+export default router
+
